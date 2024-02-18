7bfaff04657f44e96c4157c0aeb4021743150084
Hadrien Boutteville <h.boutteville@leosidea.com>
2024-02-18 15:00:38 +0100

    feat: init React Router conf with error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './main.css';
+import ErrorPage, { loader as errorLoader } from '~/routes/error-page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: null, // TODO: layout
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: null, // TODO: index page
+          },
+          {
+            path: '/*',
+            element: <></>,
+            loader: errorLoader,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
