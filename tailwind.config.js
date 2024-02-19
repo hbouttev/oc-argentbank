1f5341b691b6eac052ceef4412d470bb133c2ccc
Hadrien Boutteville <h.boutteville@leosidea.com>
2024-02-19 10:57:29 +0100

    style: add default font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Avenir', 'Helvetica', 'Arial', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
-        'primary': '#2c3e50',
-        'secondary': '#222222',
-        'dark': '#12002b',
+        primary: '#2c3e50',
+        secondary: '#222222',
+        dark: '#12002b',
       },
     },
   },
