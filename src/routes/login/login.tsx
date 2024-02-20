2ca5b660331c682252bc17bf5e2a3f6cdc6686f2
Hadrien Boutteville <h.boutteville@leosidea.com>
2024-02-21 00:59:11 +0100

    feat: add login functionality

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -1,19 +1,31 @@
 import UserIcon from '~/assets/icons/user-circle.svg?react';
-import { Form } from 'react-router-dom';
+import { Form, redirect, useActionData } from 'react-router-dom';
 import type { ActionFunctionArgs } from 'react-router-dom';
-import FormTextInput from '~/components/FormTextInput/FormTextInput.tsx';
+import FormTextInput from '~/features/auth/LoginForm/FormTextInput.tsx';
+import { login } from '~/features/auth/login.ts';
+import type { ActionData } from '~/types/react-router.ts';
 
 export async function action({ request }: ActionFunctionArgs) {
-  // handle login with a model or other and loginUser() or similar
-  // ten check in other routes with authenticated() or similar
   const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  console.log(data);
-  console.log('login action');
-  return null;
+  const { username, password } = Object.fromEntries(formData);
+  if (typeof username !== 'string' || typeof password !== 'string') {
+    throw new Error('Invalid username or password');
+  }
+  const { error } = await login(username, password);
+
+  if (error) {
+    return { error };
+  }
+
+  return redirect('/profile');
 }
 
 export default function Login() {
+  const actionData = useActionData() as ActionData<typeof action>;
+  let error = '';
+  if (actionData?.error) {
+    error = actionData.error;
+  }
   return (
     <section className="mx-auto mt-12 w-[300px] bg-white p-8">
       <UserIcon className="fill-primary inline-block size-[16px]" />
@@ -32,6 +44,7 @@ export default function Login() {
             Remember me
           </label>
         </div>
+        {error && <p className="mt-4 text-[1.1rem] text-red-500">{error}</p>}
         <button
           type="submit"
           className="bg-tertiary border-tertiary mt-4 block w-full p-2 text-[1.1rem] font-bold text-white"
