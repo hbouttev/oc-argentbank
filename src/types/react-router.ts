36f3f8fbb47c2fd28ebd8603ea970c1d1eb8a144
Hadrien Boutteville <h.boutteville@leosidea.com>
2024-02-20 18:02:52 +0100

    feat: add ActionDate type for useActionData

--- src/types/react-router.ts
+++ src/types/react-router.ts
@@ -1,7 +1,7 @@
-import { LoaderFunction } from 'react-router-dom';
+import type { LoaderFunction, ActionFunction } from 'react-router-dom';
 
-export type LoaderData<TLoaderFn extends LoaderFunction> = Awaited<
-  ReturnType<TLoaderFn>
-> extends Response | infer D
-  ? D
-  : never;
+export type LoaderData<TLoaderFn extends LoaderFunction> =
+  Awaited<ReturnType<TLoaderFn>> extends Response | infer D ? D : never;
+
+export type ActionData<TActionFn extends ActionFunction> =
+  Awaited<ReturnType<TActionFn>> extends Response | infer D ? D : never;
